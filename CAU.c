fa5c393510a5e3ca1bbf635fe5c790502db7a81c
P0ANG <129133700+P0ANG@users.noreply.github.com>
2023-06-16 23:28:12 +0900

    test

---
+++ CAU.c
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int recursive_func(int n)
+{
+    int p = 0;
+    int q = 1;
+    int fib;
+    for (int i = 0; i<n; i++)
+    {
+        if(i == 0){
+            printf("%d", p);
+        }
+        else if(i == 1){
+            printf("%d", q);
+        }
+        else{
+            fib = p + q;
+            printf("%d", fib);
+            p = q;
+            q = fib;
+        }
+    }
+}
+
+int main(void)
+{
+    int num, result;
+    scanf("%d", num);
+    result = recursive_func(num);
+
+    printf("%d th Fibonacci number = %d\n", num, result);
+
+    return 0;
+}
---
+++ CAU.py
@@ -0,0 +1,11 @@
+#제출용 GPA - F X, 열람용 GPA - F O
+
+sub_name = []
+
+sub_num =int(input("과목수를 입력하세요 : "))
+for i in range (sub_num):
+    {
+        sub_name.append(input("과목명을 입력하세요 : "))
+        
+    }
+print(sub_name)
